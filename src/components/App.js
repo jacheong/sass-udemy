f40033f79af7ee9470fef9bde5c09e4554c828aa
Justin Cheong <js388111@gmail.com>
2019-03-12 02:55:36 +0000

    section 1 complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,13 @@ import './App.scss';
 const App = () => {
     return (
         <header className="header">
-            <h1 className="heading-primary">
-                <span className="heading-primary-main">Outdoors</span>
-                <span className="heading-primary-sub">is where life happens</span>
-            </h1>
+            <div className="text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary-main">Outdoors</span>
+                    <span className="heading-primary-sub">is where life happens</span>
+                </h1>
+                <a href="#" className="btn btn-white btn-animated">Discover our tours</a>
+            </div>
         </header>
     );
 };
